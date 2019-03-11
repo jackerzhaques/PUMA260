159632c5cec58493db8cea4a3b0fb8330bc8897d
Todd Morehouse <toddmorehouse@gmail.com>
2019-03-11 13:59:17 -0400

    Initial Code Commit, Parts 2 and 3 of 3

---
+++ System Test/Debug/main.d
@@ -0,0 +1,27 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../pinout.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdbool.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/inc/hw_memmap.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/gpio.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/rom.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/sysctl.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/pin_map.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/pwm.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/adc.h
+main.obj: E:/Senior\ Design\ Project/Software/TivaWare/driverlib/ssi.h
+
+../main.c: 
+../pinout.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/inc/hw_memmap.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/gpio.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/rom.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/sysctl.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/pin_map.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/pwm.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/adc.h: 
+E:/Senior\ Design\ Project/Software/TivaWare/driverlib/ssi.h: 

f44d7c414a521100b9a34ab665dbc1550a270520
toddmorehouse <toddmorehouse@gmail.com>
2019-03-14 21:02:56 -0400

    Removing TivaWare from github.

--- Motor Driver/Single Motor Sweep/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"E:/Senior\ Design\ Project/Software/TivaWare/driverlib/ccs/Debug/driverlib.lib"
-
